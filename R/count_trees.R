508b3ed2fdc24efba83f0a9dabf97435f70135ae
hamedbh <hamedbh@users.noreply.github.com>
2020-12-04 10:25:30 +0000

    move day 3 function out to its own script

---
+++ R/count_trees.R
@@ -0,0 +1,15 @@
+count_trees <- function(map_matrix, step_down, step_right) {
+    
+    # Easier to get the indices we will need separately
+    rows <- seq(1, nrow(map_matrix), by = step_down)
+    columns <- 
+        seq(1, by = step_right, length.out = length(rows)) %% ncol(map_matrix)
+    # Replacing the zeroes is maybe a bit hacky but this is the price we pay for
+    # indexing from 1 and being unwilling to spend time on figuring out a more
+    # elegant solution with modular arithmetic
+    columns[columns == 0L] <- ncol(day03_map)
+    
+    map2_chr(rows, columns, ~ map_matrix[.x, .y]) %>% 
+        str_c(collapse = "") %>% 
+        str_count("#")
+}
